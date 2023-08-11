8903a401b74e22740cddcb69c136b261dcc15152
alexdilion <alexdilion@protonmail.com>
2023-08-11 16:31:20 +0100

    Initial project setup

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.js",
+    devtool: "inline-source-map",
+    devServer: {
+        static: "./dist",
+        watchFiles: ["src/*.html"],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Output Management",
+            template: "./src/index.html",
+        }),
+    ],
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+};
