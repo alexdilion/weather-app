e85a598c08b4e78b603fe72b48c587051b97daf8
alexdilion <alexdilion@protonmail.com>
2023-08-15 11:15:16 +0100

    Add processWeatherData function

--- src/index.js
+++ src/index.js
@@ -3,6 +3,26 @@ import "./style/index.css";
 const WEATHER_URL =
     "http://api.weatherapi.com/v1/forecast.json?key=dfe8fba030ff459b8f3141617230708&days=3";
 
+function processWeatherData(data) {
+    const { forecast, location } = data;
+
+    const locationData = {
+        name: location.name,
+        country: location.country,
+    };
+
+    const forecastData = forecast.forecastday.map((forecastDay) => ({
+        date: forecastDay.date,
+        weather: forecastDay.day.condition.text,
+        weatherImage: forecastDay.day.condition.icon,
+        maxTemp: forecastDay.day.maxtemp_c,
+        minTemp: forecastDay.day.mintemp_c,
+        avgTemp: forecastDay.day.avgtemp_c,
+        rainChance: forecastDay.day.daily_chance_of_rain,
+    }));
+
+    return { locationData, forecastData };
+}
 
 async function fetchWeather(location) {
     const requestURL = `${WEATHER_URL}&q=${location}`;
