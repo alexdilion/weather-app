25b8cadb25e046b91bbaf6a96020524ff0209615
alexdilion <alexdilion@protonmail.com>
2023-08-23 23:09:58 +0100

    Move weather functions to own module

--- src/index.js
+++ src/index.js
@@ -1,45 +1,10 @@
-import { format } from "date-fns";
-
 import "./style/index.css";
 import "./style/cards.css";
+
+import fetchWeather from "./weather";
 import elements from "./elements";
-
-const WEATHER_URL =
-    "http://api.weatherapi.com/v1/forecast.json?key=dfe8fba030ff459b8f3141617230708&days=3";
-
-function processWeatherData(data) {
-    const { forecast, location } = data;
-
-    const locationData = {
-        name: location.name,
-        country: location.country,
-    };
-
-    const forecastData = forecast.forecastday.map((forecastDay) => ({
-        date: new Date(forecastDay.date),
-        day: format(new Date(forecastDay.date), "EEEE"),
-        weather: forecastDay.day.condition.text,
-        weatherImage: forecastDay.day.condition.icon,
-        maxTemp: forecastDay.day.maxtemp_c,
-        minTemp: forecastDay.day.mintemp_c,
-        avgTemp: forecastDay.day.avgtemp_c,
-        rainChance: forecastDay.day.daily_chance_of_rain,
-    }));
-
-    console.log({ locationData, forecastData });
-
-    return { locationData, forecastData };
-}
-
-async function fetchWeather(location) {
-    const requestURL = `${WEATHER_URL}&q=${location}`;
-    const response = await fetch(requestURL, { mode: "cors" });
-    const data = await response.json();
-
-    return processWeatherData(data);
-}
 
 elements.locationForm.addEventListener("submit", (event) => {
     event.preventDefault();
-    fetchWeather(elements.locationQuery.value);
+    const data = fetchWeather(elements.locationQuery.value);
 });
---
+++ src/weather.js
@@ -0,0 +1,36 @@
+import { format } from "date-fns";
+
+const WEATHER_URL =
+    "http://api.weatherapi.com/v1/forecast.json?key=dfe8fba030ff459b8f3141617230708&days=3";
+
+function processWeatherData(data) {
+    const { forecast, location } = data;
+
+    const locationData = {
+        name: location.name,
+        country: location.country,
+    };
+
+    const forecastData = forecast.forecastday.map((forecastDay) => ({
+        date: new Date(forecastDay.date),
+        day: format(new Date(forecastDay.date), "EEEE"),
+        weather: forecastDay.day.condition.text,
+        weatherImage: forecastDay.day.condition.icon,
+        maxTemp: forecastDay.day.maxtemp_c,
+        minTemp: forecastDay.day.mintemp_c,
+        avgTemp: forecastDay.day.avgtemp_c,
+        rainChance: forecastDay.day.daily_chance_of_rain,
+    }));
+
+    console.log({ locationData, forecastData });
+
+    return { locationData, forecastData };
+}
+
+export default async function fetchWeather(location) {
+    const requestURL = `${WEATHER_URL}&q=${location}`;
+    const response = await fetch(requestURL, { mode: "cors" });
+    const data = await response.json();
+
+    return processWeatherData(data);
+}
