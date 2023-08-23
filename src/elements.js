0dd87462de0a4dda295d719d7ffdf1d3f0ba0b42
alexdilion <alexdilion@protonmail.com>
2023-08-23 23:02:26 +0100

    Add elements module

---
+++ src/elements.js
@@ -0,0 +1,15 @@
+const elements = {
+    bigCardTemplate: document.querySelector(".templates .card-big"),
+
+    main: document.querySelector("main"),
+    placeholder: document.querySelector(".placeholder"),
+    forecastsWrapper: document.querySelector("forecasts-wrapper"),
+    dailyForecasts: document.querySelector(".daily-forecasts"),
+
+    locationForm: document.querySelector("#location-form"),
+    locationQuery: document.querySelector("#location-query"),
+    searchQueryButton: document.querySelector("#search-query-button"),
+    currentLocationButton: document.querySelector("#current-location-query"),
+};
+
+export default elements;
